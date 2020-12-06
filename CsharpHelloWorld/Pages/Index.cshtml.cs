987a697e898a8931b7e6f47585d44a3f762dbf49
Esteban Zavala <esteban.zavala@solvex.com.do>
2020-12-06 16:32:50 -0400

    Initial Commit

---
+++ CsharpHelloWorld/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CsharpHelloWorld.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string HostName { get; set; }
+        public string Ip { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            this.HostName = Dns.GetHostName();
+            IPAddress[] ipaddress = Dns.GetHostAddresses(HostName);
+            this.Ip = ipaddress[0].ToString();
+        }
+    }
+}
