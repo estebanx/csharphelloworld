2f64783d6e8e183149213b7c849ba517a78f3d38
Esteban Zavala <esteban.zavala@solvex.com.do>
2020-12-08 02:13:57 -0400

    Fix docker file

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -15,6 +18,7 @@ namespace CsharpHelloWorld.Pages
 
         public string HostName { get; set; }
         public string Ip { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -23,8 +27,8 @@ namespace CsharpHelloWorld.Pages
         public void OnGet()
         {
             this.HostName = Dns.GetHostName();
-            IPAddress[] ipaddress = Dns.GetHostAddresses(HostName);
-            this.Ip = ipaddress[0].ToString();
+            var ipaddress = Dns.GetHostAddresses(HostName).Where(f => f.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+            this.Ip = ipaddress.FirstOrDefault().ToString();
         }
     }
 }
